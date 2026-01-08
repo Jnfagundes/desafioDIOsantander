a24cbfed8475783d555790b9901049521309bc99
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2026-01-08 20:28:58 -0300

    Criação da lista

---
+++ lista_usuarios.py
@@ -0,0 +1,6 @@
+users = [
+    {"id":1, "Nome": "Jailton", "News":[]},
+    {"id":2, "Nome": "José", "News":[]},
+    {"id":3, "Nome": "Tobias", "News":[]},
+    {"id":4, "Nome": "Marcelo", "News":[]},
+]
